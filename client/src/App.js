806131395fb0260a6c97f6e84692cbab17f25d37
Kamil Duvert <kduvert@gmail.com>
2021-04-03 13:30:22 +0200

    add_initial_setup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+// == Packages
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// == Components
+import Chat from './components/Chat';
+import Join from './components/Join';
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+}
+
+export default App;
